e1d34e5e2fc9bc2b069eab20220c15c650e80a14
gaYERMA <84148006+SithLor@users.noreply.github.com>
2022-11-21 01:01:28 -0800

    haha

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Ship
+https://www.youtube.com/playlist?list=PL_2VhOvlMk4UHGqYCLWc6GO8FaPl8fQTh
+https://github.com/tylerlaceby/guide-to-interpreters-series
 A program Langies
 This Coding L
 .water = varable file 
--- compiler/lexer.ts
+++ compiler/lexer.ts
@@ -6,12 +6,12 @@ export enum TokenType {
 
 	// Keywords
 	Let,
-
 	// Grouping * Operators
 	BinaryOperator,
 	Equals,
 	OpenParen,
 	CloseParen,
+	EOF,//END OF FILE
 }
 
 /**
@@ -74,7 +74,7 @@ export function tokenize(sourceCode: string): Token[] {
 		} else if (src[0] == ")") {
 			tokens.push(token(src.shift(), TokenType.CloseParen));
 		} // HANDLE BINARY OPERATORS
-		else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
+		else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/" || src[0] == "%" ) {
 			tokens.push(token(src.shift(), TokenType.BinaryOperator));
 		} // Handle Conditional & Assignment Tokens
 		else if (src[0] == "=") {
@@ -122,10 +122,6 @@ export function tokenize(sourceCode: string): Token[] {
 			}
 		}
 	}
-
+	tokens.push({type: TokenType.EOF, value:"EndOfFile"});
 	return tokens;
-}
-const source = await Deno.readTextFile("./test.txt")
-for (const token of tokenize(source)){
-	console.log(source)
 }
